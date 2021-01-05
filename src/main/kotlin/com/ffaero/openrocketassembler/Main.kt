56ec42f0e7ff0ce15ee88d11ccd1f1fa588f66c6
Zach Deibert <zachdeibert@gmail.com>
2021-01-05 10:46:16 -0600

    Reset project for bottom-up development instead of top-down

--- src/main/kotlin/com/ffaero/openrocketassembler/Main.kt
+++ src/main/kotlin/com/ffaero/openrocketassembler/Main.kt
@@ -1,13 +1,6 @@
 package com.ffaero.openrocketassembler
 
-import com.ffaero.openrocketassembler.controller.ApplicationController
-import com.ffaero.openrocketassembler.model.Application
-import com.ffaero.openrocketassembler.view.ApplicationView
-
 @Suppress("UNUSED_VARIABLE")
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-	val model = Application()
-	val controller = ApplicationController(model)
-	val view = ApplicationView(controller)
-	controller.start()
+	System.out.println("Hello, world!")
 }
--- src/main/kotlin/com/ffaero/openrocketassembler/controller/ApplicationControllerAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.ffaero.openrocketassembler.controller
-
-open class ApplicationControllerAdapter : ApplicationControllerListener {
-	override fun onStart(sender: ApplicationController) = Unit
-	override fun onStop(sender: ApplicationController) = Unit
-	override fun onProjectChange(sender: ProjectController, name: String?, modified: Boolean) = Unit
-	override fun onHistoryChange(sender: HistoryController, undoAction: String?, redoAction: String?) = Unit
-	override fun onOpenRocketVersionChange(sender: OpenRocketController, versionIndex: Int) = Unit
-}
