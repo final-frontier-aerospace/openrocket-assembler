876343ca804b55a4a0134757c94bee7fc0577624
Zach Deibert <zachdeibert@gmail.com>
2021-01-05 17:18:27 -0600

    Fixed invalid version strings

--- src/main/kotlin/com/ffaero/openrocketassembler/controller/actions/DefaultOpenRocketVersion.kt
+++ src/main/kotlin/com/ffaero/openrocketassembler/controller/actions/DefaultOpenRocketVersion.kt
@@ -6,20 +6,18 @@ import com.ffaero.openrocketassembler.controller.ProjectAdapter
 import com.ffaero.openrocketassembler.controller.OpenRocketAdapter
 
 class DefaultOpenRocketVersion(private val proj: ProjectController) : IAction {
-	private val openRocketListener = object : OpenRocketAdapter() {
-		override fun onOpenRocketVersionsUpdated(sender: OpenRocketController, versions: List<String>) {
-			if (!versions.isEmpty() && proj.openRocketVersion.isEmpty()) {
-				proj.openRocketVersion = versions.first()
-			}
+	private fun fixVersion(version: String, versions: List<String>) {
+		if ((version.isEmpty() || !versions.contains(version)) && !versions.isEmpty()) {
+			proj.openRocketVersion = versions.first()
 		}
 	}
 	
+	private val openRocketListener = object : OpenRocketAdapter() {
+		override fun onOpenRocketVersionsUpdated(sender: OpenRocketController, versions: List<String>) = fixVersion(proj.openRocketVersion, versions)
+	}
+	
 	private val projectListener = object : ProjectAdapter() {
-		override fun onOpenRocketVersionChange(sender: ProjectController, version: String) {
-			if (version.isEmpty() && !proj.app.openrocket.versions.isEmpty()) {
-				proj.openRocketVersion = proj.app.openrocket.versions.first()
-			}
-		}
+		override fun onOpenRocketVersionChange(sender: ProjectController, version: String) = fixVersion(version, proj.app.openrocket.versions)
 	}
 	
 	override fun stop() {
